76a1268e97078b326d404e775e73f5324c1589ce
Victoria Hood <vhood3@lsu.edu>
2018-03-05 03:14:22 -0600

    update

---
+++ js/code3.js
@@ -0,0 +1,11 @@
+let id = 1
+let url = 'https://www.arcgis.com/apps/MapTour/index.html?appid=cbfdf33555724fd1b5282eb373d1d31a'
+let title = 'Story Map tour Scranton'
+let myBoolean = true
+let keywords = ['apple', 'orange', 'pear']
+
+console.log('id: ' + id)
+console.log('url: ' + url)
+console.log('Title: ' + title)
+console.log('Does it have a thumbnail image? ' + hasThumbnail)
+console.log('Keywords: ' + projectKeywords)
