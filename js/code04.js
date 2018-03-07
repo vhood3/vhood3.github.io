029aed2559ccba34824ce6b78a873967d113aabc
Victoria Hood <victoriahood@danette-duponts-imac.local>
2018-03-07 01:12:56 -0600

    fhs

---
+++ js/code04.js
@@ -0,0 +1,18 @@
+let id = 1
+let url = 'https://www.arcgis.com/apps/MapTour/index.html?appid=cbfdf33555724fd1b5282eb373d1d31a'
+let title = 'Story Map tour Scranton'
+let myBoolean = true
+let keywords = ['apple', 'orange', 'pear']
+
+console.log(id)
+console.log(url)
+console.log(title)
+console.log(true)
+console.log(keywords)
+
+
+console.log('id: ' + id)
+console.log('url: ' + url)
+console.log('Title: ' + title)
+console.log('Does it have a thumbnail image? ' + hasThumbnail)
+console.log('Keywords: ' + projectKeywords)
