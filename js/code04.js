693d0d589660abcfd35820e2d266910f4e9ce7db
vhood3 <35344051+vhood3@users.noreply.github.com>
2018-04-03 10:50:15 -0500

    vsdgv

--- js/code04.js
+++ js/code04.js
@@ -1,18 +1,20 @@
-let id = 1
-let url = 'https://www.arcgis.com/apps/MapTour/index.html?appid=cbfdf33555724fd1b5282eb373d1d31a'
-let title = 'Story Map tour Scranton'
-let myBoolean = true
-let keywords = ['apple', 'orange', 'pear']
+let projectArray = [
+  1,
+  'https://www.arcgis.com/apps/MapTour/index.html?appid=cbfdf33555724fd1b5282eb373d1d31a',
+  'Story Map tour Scranton',
+  'Book Assignment 1',
+  true,
+  ['Scranton','Story Map']
+]
+let projectObject = {
+  id: 1,
+  url = 'https://www.arcgis.com/apps/MapTour/index.html?appid=cbfdf33555724fd1b5282eb373d1d31a',
+  title = 'Story Map tour Scranton',
+  desc = 'Book Assignment 1',
+  thumb: true,
+  keywords: ['Scranton','Story Map']
+}
 
-console.log(id)
-console.log(url)
-console.log(title)
-console.log(true)
-console.log(keywords)
-
-
-console.log('id: ' + id)
-console.log('url: ' + url)
-console.log('Title: ' + title)
-console.log('Does it have a thumbnail image? ' + hasThumbnail)
-console.log('Keywords: ' + projectKeywords)
+console.log(projectArray[0], projectObject.id, projectObject["id"])
+console.log(projectArray[1], projectObject.url, projectObject["url"])
+console.log(projectArray[2], projectObject.title, projectObject["title"])
---
+++ js/code05.js
@@ -0,0 +1,25 @@
+let projectObject1 = {
+  id: 1,
+  url = 'https://www.arcgis.com/apps/MapTour/index.html?appid=cbfdf33555724fd1b5282eb373d1d31a',
+  title = 'Story Map tour Scranton',
+  desc = 'Book Assignment 1',
+  thumb: true,
+  keywords: ['Scranton','Story Map']
+}
+
+let projectObject2 = {
+  id: 2,
+  url = 'http://arcg.is/0KPn8a',
+  title = 'Recent Earthquakes Worldwide',
+  desc = 'Book Assignment 2',
+  thumb: true,
+  keywords: ['recent earthquakes']
+}
+
+let projectObjects = [
+  projectObject1,
+  projectObject2
+]
+
+for (let i = 0; i < projectObjects.length; i++) {
+  console.log('Element ' + i + ' title: ' + projectObjects[i].title)
