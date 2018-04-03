b5724e21fe1eac4abc667cbd4880ed9121642585
vhood3 <35344051+vhood3@users.noreply.github.com>
2018-04-03 11:38:59 -0500

    jyg

--- map/webmap-assignment3.js
+++ map/webmap-assignment3.js
@@ -6,14 +6,14 @@ subdomains: 'abcd',
 ext: 'png'
 }).addTo(map)
 
-let courage = L.icon({
-  iconUrl: 'images/purpleDog.PNG',
+let dinah = L.icon({
+  iconUrl: 'images/dinah.jpg',
   iconSize: [68, 78], // size of the icon
   iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
   popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
 })
 
-let marker = L.marker([51.5, -0.09], {icon:courage}).addTo(map)
+let marker = L.marker([51.5, -0.09], {icon:dinah}).addTo(map)
 let polygon = L.polygon([
   [51.509, -0.08],
   [51.503, -0.06],
