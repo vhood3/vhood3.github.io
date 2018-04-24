d88ff5860dda005ad7c619b81bd18988d2adc623
Victoria Hood <victoriahood@a4-5e-60-c3-eb-a5.wlan.lsu.edu>
2018-04-23 22:55:56 -0500

    hjhk

--- map/webmap-assignment4.js
+++ map/webmap-assignment4.js
@@ -2,17 +2,17 @@ let map = L.map('VictoriaMap').setView([20.755035, -155.983552], 13)
 
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png').addTo(map)
 
-geoJSONstyle = function (states) {
+mygeoJSONstyle = function (states) {
   let age = states.properties.MED_AGE
   let statecolor = 'Green'
   if ( age < 38 ) {statecolor = 'Blue'}
-  let Style = {
+  let mygeoJSONstyle = {
     color: statecolor,
     weight: 2,
     fillOpacity: 0.2,
     opacity: 0.3,
   }
-  return Style
+  return mygeoJSONstyle
 }
 
 function createPopup (states, statelayer) {
@@ -22,8 +22,8 @@ function createPopup (states, statelayer) {
   statelayer.bindPopup('Population of ' + name + ':' + population + '<br>Median age ' + ': ' + age + '<br>National average: 38')
 }
 
-geoJSONoptions = {
-  style: geoJSONstyle,
+let geoJSONoptions = {
+  style: mygeoJSONstyle,
   onEachFeature: createPopup
 }
 
